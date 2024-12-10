77d9c95aeb26f8b0be87d4b165460f4dd6186427
Finodya Yahdun <114790219+finodya27@users.noreply.github.com>
2024-12-10 21:39:37 +0700

    Update corsMiddleware.js

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,15 +1,9 @@
-const corsMiddleware = {
-  method: 'onPreResponse',
-  assign: 'corsHandler',
-  handler: (request, h) => {
-    const response = request.response;
-    if (!response.isBoom) {
-      response.header('Access-Control-Allow-Origin', '*');
-      response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-      response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    }
-    return h.continue;
-  },
-};
+const cors = require("cors");
+
+const corsMiddleware = cors({
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
 
 module.exports = { corsMiddleware };
