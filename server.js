49f613c20f701ebccc768be55445655fb994d800
Finodya Yahdun <114790219+finodya27@users.noreply.github.com>
2024-12-10 12:51:31 +0700

    Update

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const Hapi = require('@hapi/hapi');
 const { errorMiddleware } = require("./middlewares/errorMiddleware");
+const { corsMiddleware } = require("./middlewares/corsMiddleware");
 const historyRoute = require("./routes/historyRoute");
 const predictRoute = require("./routes/predictRoute");
 
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 8080,
-    host: 'localhost',
+    host: '0.0.0.0',
   });
 
+  // Menambahkan route
   server.route(predictRoute);
   server.route(historyRoute);
 
-  // Error handling
+  // Middleware untuk menangani CORS
+  server.ext(corsMiddleware.method, corsMiddleware.handler);
+
+  // Middleware untuk error handling
   server.ext('onPreResponse', errorMiddleware);
 
+  // Menjalankan server
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
 };
