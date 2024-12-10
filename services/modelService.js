4348052e189b332d0d773af7fa73ecd073fae7e2
Finodya Yahdun <114790219+finodya27@users.noreply.github.com>
2024-12-10 21:41:58 +0700

    Update modelService.js

--- services/modelService.js
+++ services/modelService.js
@@ -3,26 +3,22 @@ const tf = require("@tensorflow/tfjs-node");
 const modelUrl = "https://storage.googleapis.com/submissionmlgc-finodyayahdun/submissions-model/model.json";
 let model;
 
-const loadModel = async () => {
+(async () => {
   model = await tf.loadGraphModel(modelUrl);
   console.log("Model loaded successfully");
-};
-
-loadModel();
+})();
 
 const predictImage = async (buffer) => {
   const imageTensor = tf.node
     .decodeJpeg(buffer)
-    .resizeNearestNeighbor([224, 224])
+    .resizeNearestNeighbor([224, 224])  // Sesuaikan ukuran input sesuai dengan model Anda
     .expandDims()
     .toFloat();
 
   const prediction = model.predict(imageTensor).dataSync();
-  const isCancer = prediction[0] > 0.5;
-
   return {
-    result: isCancer ? "Cancer" : "Non-cancer",
-    suggestion: isCancer ? "Segera periksa ke dokter!" : "Penyakit kanker tidak terdeteksi.",
+    result: prediction[0] > 0.5 ? "Cancer" : "Non-cancer",
+    suggestion: prediction[0] > 0.5 ? "Consult a doctor!" : "No cancer detected.",
   };
 };
 
