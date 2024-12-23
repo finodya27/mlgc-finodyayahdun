8f9fc7b871336af0e6fc4198b6b82ddca397b0c5
Finodya Yahdun <114790219+finodya27@users.noreply.github.com>
2024-12-23 09:04:08 +0700

    Update uploadMiddleware.js

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,15 +1,8 @@
 const multer = require("multer");
 
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage,
-  limits: { fileSize: 1_000_000 }, // Batas ukuran 1MB
-  fileFilter: (req, file, cb) => {
-    if (!file.mimetype.startsWith("image/")) {
-      return cb(new Error("Only image files are allowed"));
-    }
-    cb(null, true);
-  },
+const uploadMiddleware = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 1000000 },
 });
 
-module.exports = { upload };
+module.exports = { uploadMiddleware };
